3b7c93b6a9661055a489e316e384d1fc88590608
cnniew <162931146+cnniew@users.noreply.github.com>
2024-06-16 01:35:57 -0400

    Hero section

--- connection.php
+++ connection.php
@@ -4,4 +4,3 @@ $connection = new mysqli($servername, $username, $password, $dbname);
 if ($connection->connect_error) {
   die("Connection failed: " . $connection->connect_error);
 }
-// echo "Connected successfully";
--- index.php
+++ index.php
@@ -9,13 +9,15 @@
 <body>
   <main>
     <section class="hero-section">
-      <h1 class="hero__header">Summer styles are finally here</h1>
-      <h2 class="hero__subheader">This year, our new summer collection will be your haven from the world's harsh elements.</h2>
-      <a class="hero__cta" href="https://www.greatfrontend.com/">Shop now</a>
+      <div>
+        <h1 class="hero__header">Summer styles are finally here</h1>
+        <h2 class="hero__subheader">This year, our new summer collection will be your haven from the world's harsh elements.</h2>
+        <a class="hero__cta" href="https://www.greatfrontend.com/">Shop now</a>
+      </div>
       <img class="hero__img" src="img/woman-riding-escalator.jpg" alt="">
     </section>
 
-    <section class="latest-arrivals-container">
+    <section class="latest-arrivals-section">
       <div class="latest-arrivals__header">
         <h3 class="latest-arrivals__title">Latest Arrivals</h3>
         <a class="latest-arrivals__view-all" href="https://www.greatfrontend.com/">View all</a>
