c515d3b43e91d577415a0c13c0eac177f46838bb
cnniew <162931146+cnniew@users.noreply.github.com>
2024-06-17 15:43:01 -0400

    Commitment section

--- head.php
+++ head.php
@@ -8,6 +8,8 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 <link href="https://fonts.googleapis.com/css2?family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet" />
 
+<link href="https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.css" rel="stylesheet" />
+
 <link rel="stylesheet" type="text/css" href="css/reset.css?<?php echo time(); ?>" />
 <link rel="stylesheet" type="text/css" href="css/global.css?<?php echo time(); ?>" />
 <link rel="stylesheet" type="text/css" href="css/style.css?<?php echo time(); ?>" />
--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 <html lang="en">
 
 <head>
-  <title>StyleNest</title>
-  <?php include 'header.php' ?>
+  <title>StyleNest Home</title>
+  <?php include 'head.php' ?>
 </head>
 
 <body>
@@ -72,6 +72,36 @@
         ?>
       </div>
     </section>
+
+    <section class="commitment-section">
+      <div class="commitment__header">
+        <h4 class="commitment__subtitle">Elevate Your Experience</h4>
+        <h3 class="commitment__title">Our Commitment to Exceptional Service</h3>
+        <p class="commitment__description">We pride ourselves on a foundation of exceptional customer service, where every interaction is a testament to our dedication to excellence.</p>
+      </div>
+
+      <div class="commitment__listing">
+        <div class="commitment__feature">
+          <i class="ri-truck-line ri-xl commitment__feature-icon"></i>
+          <h5 class="commitment__feature-title">Complimentary Shipping</h5>
+          <p class="commitment__feature-description">Enjoy the convenience of free shipping for all orders. We believe in transparent pricing, and the price you see is the price you pay—no surprise fees.</p>
+        </div>
+        <div class="commitment__feature">
+          <i class="ri-shield-check-line ri-xl commitment__feature-icon"></i>
+          <h5 class="commitment__feature-title">2-Year Quality Promise</h5>
+          <p class="commitment__feature-description">Shop with confidence knowing that we stand behind our products. Should any issue arise within the first two years, rest assured we're here to help with a hassle-free replacement.</p>
+        </div>
+        <div class="commitment__feature">
+          <i class="ri-exchange-line ri-xl commitment__feature-icon"></i>
+          <h5 class="commitment__feature-title">Easy Exchanges</h5>
+          <p class="commitment__feature-description">If your purchase isn't quite right, pass it on to a friend who might love it, and let us know. We're happy to facilitate an exchange to ensure you have the perfect item to complement your lifestyle.</p>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+
+    </footer>
   </main>
 
 </body>
